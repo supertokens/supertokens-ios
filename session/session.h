fe81b660bba1a0a2f446f79642618a561b3f3053
Nemi Shah <nemishah1212@gmail.com>
2019-09-20 11:14:42 +0530

    Initial Commit

---
+++ session/session.h
@@ -0,0 +1,19 @@
+//
+//  session.h
+//  session
+//
+//  Created by Nemi Shah on 20/09/19.
+//  Copyright © 2019 SuperTokens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for session.
+FOUNDATION_EXPORT double sessionVersionNumber;
+
+//! Project version string for session.
+FOUNDATION_EXPORT const unsigned char sessionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <session/PublicHeader.h>
+
+
