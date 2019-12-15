c56ee180314adb9ee69d67dcda184abf4c425942
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-12-15 23:14:27 +0530

    change to testing script

--- .circleci/setupAndTestWithCommercialCore.sh
+++ .circleci/setupAndTestWithCommercialCore.sh
@@ -73,4 +73,3 @@ npm i git+https://github.com:supertokens/supertokens-node.git#$2
 JAVA=/Users/travis/build/supertokens/jdk-12.0.2.jdk/Contents/Home/bin/java TEST_MODE=testing INSTALL_PATH=../../../com-root node ./build/index.js &
 cd ../../
 xcodebuild test -project session.xcodeproj -scheme sessionTests -destination 'platform=iOS Simulator,name=iPhone 11 Pro'
-cd .circleci
