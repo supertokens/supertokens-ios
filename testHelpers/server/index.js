48d6110a4c50e05a12a67889a16465f416f04088
Nemi Shah <nemishah1212@gmail.com>
2023-09-22 13:36:24 +0530

    Fix tests (#54)

--- testHelpers/server/index.js
+++ testHelpers/server/index.js
@@ -51,6 +51,7 @@ app.use(cookieParser());
 
 let lastSetEnableAntiCSRF = false;
 let lastSetEnableJWT = false;
+let accountLinkingSupported = maxVersion(supertokens_node_version, "15.0") === supertokens_node_version;
 
 function getConfig(enableAntiCsrf, enableJWT, jwtPropertyName) {
     if (maxVersion(supertokens_node_version, "14.0") === supertokens_node_version && enableJWT) {
@@ -211,7 +212,7 @@ app.post("/login", async (req, res) => {
     
     let session;
     if (multitenancySupported) {
-        session = await Session.createNewSession(req, res, "public", userId);
+        session = await Session.createNewSession(req, res, "public", accountLinkingSupported ? SuperTokens.convertToRecipeUserId(userId) : userId);
     } else {
         session = await Session.createNewSession(req, res, userId);
     }
